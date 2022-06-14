3b4906062e36554522c2b9b5a9552ac1e3607b85
bilguun1324 <bilguun1324@gmail.com>
2022-06-14 23:01:12 +0800

    last change

--- schemas/Group.ts
+++ schemas/Group.ts
@@ -2,6 +2,7 @@ import mongoose, { Schema } from "mongoose";
 
 const GroupSchema = new Schema({
     name: { type: String, maxlength: 50, minlength: 2 },
+    description: { type: String, maxlength: 255, minlength: 2 },
     members: { type: [{ uid: String, permission: String }] },
 });
 
